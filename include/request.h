e64c432e20d76da4d7ce14e57db04646eeb50d4c
Travis Avey <travis.avey@pm.me>
2024-01-25 17:03:52 -0500

    not working ATM, need to get CMake to build with libcurl correctly still

--- include/request.h
+++ include/request.h
@@ -1,6 +1,26 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+
+#include <curl/curl.h>
 /*
  *  request.h
  *
- *
+ *  Basic Usage for GET, POST, PUT, PATCH, DELETE
  *
  */
+
+typedef struct {
+  CURL *curl;
+  long code;
+  char *url;
+  char *text;
+} request;
+
+// initialize the library
+// returns 0 on success, -1 if fail
+int init(request *);
+
+// close the libCURL interface
+void close(request *);
+
+#endif
--- src/request.c
+++ src/request.c
@@ -4,3 +4,34 @@
  *
  *
  */
+
+#include "../include/request.h"
+#include <stdlib.h>
+
+// init the libCURL
+// if failed returns -1
+// success: 0
+int init(request *req) {
+  req->url = NULL;
+  req->text = NULL;
+  req->code = 0;
+
+  req->url = calloc(1, 1);
+  if (req->url == NULL) {
+    return -1;
+  }
+
+  req->text = calloc(1, 1);
+  if (req->text == NULL) {
+    return -1;
+  }
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  req->curl = curl_easy_init();
+  if (req->curl) {
+    return 0;
+  }
+  return -1;
+}
+
+void close(request *req) { curl_easy_cleanup(req->curl); }
