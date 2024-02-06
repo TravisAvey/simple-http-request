99f0bf6f639a363b07689a327b57f53685ab643d
Travis Avey <travis.avey@pm.me>
2024-02-06 16:55:00 +0000

    Resolve "Add DELETE Request"

---
+++ examples/delete.c
@@ -0,0 +1,22 @@
+
+/*
+ *   Show simple example of a DELETE request
+ *
+ *
+ */
+
+#include "../include/request.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  request req;
+  simpleHttpInit();
+  req.url = "https://httpbin.org/delete";
+  int res = simpleHttpDelete(&req, JSON);
+  printf("HTTP Response code: %ld\n", req.code);
+  printf("req->data: %s\n", req.body);
+  simpleHttpClose();
+
+  return 0;
+}
