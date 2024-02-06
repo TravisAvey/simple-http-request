62ebdcd01ed94212512724a66d96fb05bb38640d
Travis Avey <travis.avey@pm.me>
2024-02-06 17:32:18 +0000

    Resolve "Add PATCH Request"

---
+++ examples/patch.c
@@ -0,0 +1,21 @@
+#include "../include/request.h"
+
+int main(int argc, char *argv[]) {
+
+  request req;
+
+  simpleHttpInit();
+
+  // req.text = "some text for post";
+  req.url = "https://httpbin.org/patch";
+  // data to update
+  req.text = "{ \"name\": \"Jim Halpert\"}";
+
+  simpleHttpPatch(&req, JSON);
+
+  printf("Response Code: %ld\n", req.code);
+  printf("Response: %s\n", req.body);
+
+  simpleHttpClose();
+  return 0;
+}
