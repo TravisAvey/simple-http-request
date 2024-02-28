51ab8060eb87bdacaa0e906ed80c4ab28a2787b8
Travis Avey <travis.avey@pm.me>
2024-02-28 09:57:59 -0500

    adding in minunit for simple testing

--- test/tests.c
+++ test/tests.c
@@ -5,6 +5,27 @@
  */
 
 #include "../include/request.h"
+#include "minunit/minunit.h"
+
+static int foo;
+static int bar;
+
+void setup(void) {
+  foo = 1;
+  bar = 8;
+}
+
+void teardown(void) {
+  // cleanup
+}
+
+MU_TEST(testing) { mu_check(foo == 1); }
+
+MU_TEST_SUITE(testSuite) {
+  MU_SUITE_CONFIGURE(&setup, &teardown);
+
+  MU_RUN_TEST(testing);
+}
 
 int main() {
   request req;
@@ -14,5 +35,8 @@ int main() {
 
   simpleHttpClose(&req, &res);
 
-  return 0;
+  MU_RUN_SUITE(testSuite);
+  MU_REPORT();
+
+  return MU_EXIT_CODE;
 }
