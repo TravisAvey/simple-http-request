246b0fd91fc9d5cf171aca6ddea06c28b3347892
Travis Avey <travis.avey@pm.me>
2024-02-28 14:49:16 -0500

    Added more tests
    Should add a few that will fail from the server

--- test/tests.c
+++ test/tests.c
@@ -5,9 +5,9 @@
 
 const char *getUrl = "https://8r7q4.wiremockapi.cloud/get/1";
 const char *postUrl = "https://8r7q4.wiremockapi.cloud/json";
-const char *putUrl = "https://httpbin.org/put";
-const char *patchUrl = "https://httpbin.org/patch";
-const char *deleteUrl = "https://httpbin.org/delete";
+const char *putUrl = "https://8r7q4.wiremockapi.cloud/json/2";
+const char *patchUrl = "https://8r7q4.wiremockapi.cloud/address/8";
+const char *deleteUrl = "https://8r7q4.wiremockapi.cloud/post/9";
 // postman:password
 // Authorization: Basic cG9zdG1hbjpwYXNzd29yZA==
 const char *authUrl = "https://postman-echo.com/digest-auth";
@@ -43,10 +43,30 @@ MU_TEST(GetRequest) {
   mu_assert_string_eq(res.body, "here is some text");
 }
 
+MU_TEST(GetAuthRequest) {
+  req.url = authUrl;
+
+  const char *headers[] = {"Authorization: Basic cG9zdG1hbjpwYXNzd29yZA=="};
+  req.headers = headers;
+  req.numHeaders = sizeof(headers) / sizeof(char *);
+
+  const char *userpass = "postman:password";
+  simpleHttpSetPassword(&req, userpass, DIGEST);
+
+  const char *expectedRes = readFile("auth.txt");
+
+  error err = simpleHttpRequest(&req, &res, TEXT, GET);
+
+  mu_assert(err == NO_ERROR, "GET request returned an error");
+  mu_assert(res.code == 200, "GET request did not return 200");
+  mu_assert_string_eq(expectedRes, res.body);
+}
+
 MU_TEST_SUITE(GetTestSuite) {
   MU_SUITE_CONFIGURE(&setup, &teardown);
 
   MU_RUN_TEST(GetRequest);
+  MU_RUN_TEST(GetAuthRequest);
 }
 
 MU_TEST(PostRequest) {
@@ -68,11 +88,70 @@ MU_TEST_SUITE(PostTestSuite) {
   MU_RUN_TEST(PostRequest);
 }
 
+MU_TEST(PutRequest) {
+  req.url = putUrl;
+  req.text = "{ \"name\": \"Jim Halpert\" }";
+
+  const char *expectedRes = readFile("put.txt");
+
+  error err = simpleHttpRequest(&req, &res, JSON, PUT);
+
+  mu_assert(err == NO_ERROR, "PUT request returned an error");
+  mu_assert(res.code == 200, "PUT request did not return 200");
+  mu_assert_string_eq(expectedRes, res.body);
+}
+
+MU_TEST_SUITE(PutTestSuite) {
+  MU_SUITE_CONFIGURE(&setup, &teardown);
+
+  MU_RUN_TEST(PutRequest);
+}
+
+MU_TEST(PatchRequest) {
+  req.url = patchUrl;
+  req.text = "{ \"address\": \"123 anywhere street\" }";
+
+  const char *expectedRes = readFile("put.txt");
+
+  error err = simpleHttpRequest(&req, &res, JSON, PATCH);
+
+  mu_assert(err == NO_ERROR, "PATCH request returned an error");
+  mu_assert(res.code == 200, "PATCH request did not return 200");
+  mu_assert_string_eq(expectedRes, res.body);
+}
+
+MU_TEST_SUITE(PatchTestSuite) {
+  MU_SUITE_CONFIGURE(&setup, &teardown);
+
+  MU_RUN_TEST(PatchRequest);
+}
+
+MU_TEST(DeleteRequest) {
+  req.url = deleteUrl;
+
+  const char *expectedRes = readFile("delete.txt");
+
+  error err = simpleHttpRequest(&req, &res, JSON, DELETE);
+
+  mu_assert(err == NO_ERROR, "DELETE request returned an error");
+  mu_assert(res.code == 200, "DELETE request did not return 200");
+  mu_assert_string_eq(expectedRes, res.body);
+}
+
+MU_TEST_SUITE(DeleteTestSuite) {
+  MU_SUITE_CONFIGURE(&setup, &teardown);
+
+  MU_RUN_TEST(DeleteRequest);
+}
+
 int main() {
 
   MU_RUN_SUITE(InitSuite);
   MU_RUN_SUITE(GetTestSuite);
   MU_RUN_SUITE(PostTestSuite);
+  MU_RUN_SUITE(PutTestSuite);
+  MU_RUN_SUITE(PatchTestSuite);
+  MU_RUN_SUITE(DeleteTestSuite);
 
   MU_REPORT();
 
